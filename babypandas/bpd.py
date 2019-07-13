ad9638b4214a1a394a81d74dd00e8347f037f99a
Darren Liu <dliu9999@gmail.com>
2019-07-13 16:19:52 -0700

    added *vargs to __array__

--- babypandas/bpd.py
+++ babypandas/bpd.py
@@ -159,7 +159,7 @@ class Series(object):
         return self._pd.__len__()
 
     # array interface (for applying numpy functions)
-    def __array__(self):
+    def __array__(self, *vargs):
         return self._pd.values
 
     # return the underlying Series
