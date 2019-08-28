f1d3822d7e72102badf32b3cd87e49ea7f0671fb
Darren Liu <dliu9999@gmail.com>
2019-08-28 15:16:42 -0700

    first travis test

--- tests/test_df.py
+++ tests/test_df.py
@@ -5,4 +5,4 @@ import bpd
 def test_df_length():
 	df = bpd.DataFrame().assign(name=['Al', 'Bob', 'Jill', 'Sally'],
 								age=['20', '25', '22', '23'])
-	assert(df.shape[0] == 4)
+	assert(df.shape[0] == 4), 'Length incorrect'
