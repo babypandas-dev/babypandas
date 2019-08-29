e9e7744ee370d20de232c05c41e03f263fbe78b3
Darren Liu <dliu9999@gmail.com>
2019-08-29 13:16:41 -0700

    debugging travis test

--- tests/test_df.py
+++ tests/test_df.py
@@ -6,3 +6,8 @@ def test_df_length():
 	df = bpd.DataFrame().assign(name=['Al', 'Bob', 'Jill', 'Sally'],
 								age=['20', '25', '22', '23'])
 	assert(df.shape[0] == 4), 'Length incorrect'
+
+def table1():
+	return bpd.DataFrame().assign(letter=['a', 'b', 'c'],
+		 						  count=[9, 3, 3],
+		 						  points=[1, 2, 2])
